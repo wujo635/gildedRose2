c6af7c8d91e40fc493f6505977ec0513e749cdd8
Johnny Wu <wujo635@gmail.com>
2019-03-23 19:16:01 -0400

    Added BackstagePassUpdater

---
+++ src/main/java/com/gildedrose/BackstagePassUpdater.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+public class BackstagePassUpdater implements ItemUpdater {
+
+    @Override
+    public void update(Item item) {
+        if (item.sellIn > 10) {
+            item.quality++;
+        } else if (item.sellIn > 5) {
+            item.quality += 2;
+        } else if (item.sellIn > 0) {
+            item.quality += 3;
+        } else {
+            item.quality = 0;
+        }
+        item.sellIn--;    }
+}
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -19,16 +19,8 @@ class GildedRose {
             updater = new AgedBrieUpdater();
             updater.update(item);
         } else if (isBackstagePass(item)) {
-            if (item.sellIn > 10) {
-                item.quality++;
-            } else if (item.sellIn > 5) {
-                item.quality += 2;
-            } else if (item.sellIn > 0) {
-                item.quality += 3;
-            } else {
-                item.quality = 0;
-            }
-            item.sellIn--;
+            updater = new BackstagePassUpdater();
+            updater.update(item);
         } else if (isSulfuras(item)) {
             item.quality = 80;
         } else {
